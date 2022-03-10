55415325d7b8274af4a1efa9d80974d566db6025
Amir Reza Mehbakhsh <theamirrezam75@gmail.com>
2022-03-10 20:57:34 +0330

    Interacting with DOM elements

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,23 @@
 "use strict";
 class ProjectForm {
     constructor() {
-        this.template = document.getElementById("project-form");
+        this.template = document.getElementById("template");
         this.container = document.getElementById("app");
         this.form = document.importNode(this.template.content, true)
             .firstElementChild;
+        this.form.id = "project-form";
+        this.title = this.form.querySelector("#title");
+        this.description = this.form.querySelector("#description");
+        this.people = this.form.querySelector("#people");
         this.attach();
+        this.configure();
+    }
+    submit(e) {
+        e.preventDefault();
+        console.log(this.title.value);
+    }
+    configure() {
+        this.form.addEventListener("submit", this.submit);
     }
     attach() {
         this.container.insertAdjacentElement("afterbegin", this.form);
--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,39 @@ class ProjectForm {
   container: HTMLDivElement;
   template: HTMLTemplateElement;
   form: HTMLFormElement;
+  title: HTMLInputElement;
+  description: HTMLInputElement;
+  people: HTMLInputElement;
 
   constructor() {
-    this.template = document.getElementById(
-      "project-form"
-    )! as HTMLTemplateElement;
+    this.template = document.getElementById("template")! as HTMLTemplateElement;
 
     this.container = document.getElementById("app")! as HTMLDivElement;
 
     this.form = document.importNode(this.template.content, true)
       .firstElementChild as HTMLFormElement;
- 
+    this.form.id = "project-form";
+
+    this.title = this.form.querySelector("#title") as HTMLInputElement;
+
+    this.description = this.form.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+
+    this.people = this.form.querySelector("#people") as HTMLInputElement;
+
     this.attach();
+
+    this.configure();
+  }
+
+  private submit(e: Event) {
+    e.preventDefault();
+    console.log(this.title.value);
+  }
+
+  private configure() {
+    this.form.addEventListener("submit", this.submit);
   }
 
   attach() {
