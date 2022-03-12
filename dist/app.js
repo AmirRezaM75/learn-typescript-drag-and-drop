102adbe33d8989b04b4a6082d9a3036fbaf44e2c
Amir Reza Mehbakhsh <theamirrezam75@gmail.com>
2022-03-12 23:52:44 +0330

    Create reusable validator

--- dist/app.js
+++ dist/app.js
@@ -10,7 +10,7 @@ function autobind(target, propertyKey, descriptor) {
         configurable: true,
         get() {
             return descriptor.value.bind(this);
-        }
+        },
     };
 }
 class ProjectForm {
@@ -28,7 +28,28 @@ class ProjectForm {
     }
     submit(e) {
         e.preventDefault();
-        console.log(this.title.value);
+        const validator = new Validator();
+        validator.make({
+            title: this.title.value,
+            description: this.description.value,
+            people: this.people.value,
+        }, {
+            title: "required|max:10",
+            description: "required|min:10|max:50",
+            people: "required|max:5",
+        });
+        if (validator.fails()) {
+            alert("This given data is invalid");
+        }
+        else {
+            console.log("sucess");
+            this.reset();
+        }
+    }
+    reset() {
+        this.title.value = "";
+        this.description.value = "";
+        this.people.value = "";
     }
     configure() {
         this.form.addEventListener("submit", this.submit);
@@ -40,4 +61,58 @@ class ProjectForm {
 __decorate([
     autobind
 ], ProjectForm.prototype, "configure", null);
+class Validator {
+    constructor() {
+        this.data = {};
+        this.rules = {};
+    }
+    make(data, rules) {
+        this.data = data;
+        this.rules = rules;
+        return this;
+    }
+    fails() {
+        let fails = false;
+        Object.keys(this.data).forEach((key) => {
+            if (this.rules[key]) {
+                const rules = this.rules[key].split("|");
+                rules.forEach((rule) => {
+                    if (rule.includes(":")) {
+                        const [statute, value] = rule.split(":");
+                        if (this[statute] &&
+                            this[statute](this.data[key], +value) === false) {
+                            fails = true;
+                        }
+                    }
+                    if (this[rule] &&
+                        /* @ts-ignore */
+                        this[rule](this.data[key]) === false) {
+                        fails = true;
+                    }
+                });
+            }
+        });
+        return fails;
+    }
+    required(value) {
+        console.log(value.length);
+        return value.length > 0;
+    }
+    min(value, minimum) {
+        if (typeof value === "string") {
+            return value.length >= minimum;
+        }
+        else {
+            return value >= minimum;
+        }
+    }
+    max(value, maximum) {
+        if (typeof value === "string") {
+            return value.length <= maximum;
+        }
+        else {
+            return value <= maximum;
+        }
+    }
+}
 new ProjectForm();
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-function autobind(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+function autobind(
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) {
   return {
-      configurable: true,
-      get() {
-          return descriptor.value.bind(this)
-      }
-  }
+    configurable: true,
+    get() {
+      return descriptor.value.bind(this);
+    },
+  };
 }
 
 class ProjectForm {
@@ -39,7 +43,34 @@ class ProjectForm {
 
   private submit(e: Event) {
     e.preventDefault();
-    console.log(this.title.value);
+
+    const validator = new Validator();
+
+    validator.make(
+      {
+        title: this.title.value,
+        description: this.description.value,
+        people: this.people.value,
+      },
+      {
+        title: "required|max:10",
+        description: "required|min:10|max:50",
+        people: "required|max:5",
+      }
+    );
+
+    if (validator.fails()) {
+      alert("This given data is invalid");
+    } else {
+      console.log("sucess");
+      this.reset();
+    }
+  }
+
+  private reset() {
+    this.title.value = "";
+    this.description.value = "";
+    this.people.value = "";
   }
 
   @autobind
@@ -49,6 +80,78 @@ class ProjectForm {
 
   attach() {
     this.container.insertAdjacentElement("afterbegin", this.form);
+  }
+}
+
+type Rule = "required" | "max" | "min";
+
+type Validatable = Record<string, string>;
+
+class Validator {
+  private data = {} as Validatable;
+
+  private rules = {} as Validatable;
+
+  public make(data: Validatable, rules: Validatable) {
+    this.data = data;
+
+    this.rules = rules;
+
+    return this;
+  }
+
+  public fails() {
+    let fails = false;
+
+    Object.keys(this.data).forEach((key) => {
+      if (this.rules[key]) {
+        const rules = this.rules[key].split("|") as Rule[];
+
+        rules.forEach((rule) => {
+          if (rule.includes(":")) {
+            const [statute, value] = rule.split(":") as [Rule, string];
+
+            if (
+              this[statute] &&
+              this[statute](this.data[key], +value) === false
+            ) {
+              fails = true;
+            }
+          }
+
+          if (
+            this[rule] &&
+            /* @ts-ignore */
+            this[rule](this.data[key]) === false
+          ) {
+            fails = true;
+          }
+        });
+      }
+    });
+
+    return fails;
+  }
+
+  private required(value: string) {
+    console.log(value.length);
+    return value.length > 0;
+  }
+
+  private min(value: string | number, minimum: number) {
+    if (typeof value === "string") {
+      return value.length >= minimum;
+    } else {
+      return value >= minimum;
+    }
+  }
+
+  private max(value: string | number, maximum: number) {
+    if (typeof value === "string") {
+      return value.length <= maximum;
+    } else {
+      return value <= maximum;
+    }
   }
 }
 
